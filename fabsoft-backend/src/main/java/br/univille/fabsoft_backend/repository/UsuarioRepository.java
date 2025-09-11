7864e8636ef900efef470d306b9fc86a5741389e
Reidner Marques <reidner.marques@univille.br>
2025-09-11 13:34:14 +0000

    mend

--- fabsoft-backend/src/main/java/br/univille/fabsoft_backend/repository/UsuarioRepository.java
+++ fabsoft-backend/src/main/java/br/univille/fabsoft_backend/repository/UsuarioRepository.java
@@ -4,8 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import br.univille.fabsoft_backend.entity.Usuario;
+import java.util.List;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-
+    List<Usuario> findById(long id);
 }
